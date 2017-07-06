37c4e366bb0b55b93718588e6311d00c901bef83
mhakanda <mhamid_93@yahoo.com>
2017-07-05 21:11:52 -0500

    last commit2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const port  = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/public'));
-// app.get('/',(req,res)=>{
-// res.send(' ')
-// });
+app.get('/',(req,res)=>{
+res.send(' ');
+});
 
 app.listen(port)
